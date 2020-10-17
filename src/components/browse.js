1ed8b93258f0b6a1d05ed0021a3dc7a2d941fd68
Bhargav <bhargav.ram@prolim.com>
2020-10-17 21:18:12 +0530

    updating the project

--- src/components/browse.js
+++ src/components/browse.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
 import React, { Component } from "react";
-import DatePicker from "react-bootstrap-date-picker";
+import DatePicker from "react-datepicker";
 
 const formvalid = formErrors => {
     let valid = true;
@@ -19,18 +19,16 @@ export default class Browser extends Component {
             event_details: null,
             event_type: null,
             location: null,
-            start_time: null,
-            end_time: null,
+            start_time: new Date(),
+            end_time: new Date(),
             formErrors: {
                 event_name: "",
                 event_details: "",
                 event_type: "",
-                location: "",
-                start_time: "",
-                end_time: ""
+                location: ""
             },
             events: []
-        }
+        };
     }
 
     handleErrors = (event) => {
@@ -54,7 +52,6 @@ export default class Browser extends Component {
                 break;
         }
         this.setState({ [name]: value });
-        console.log(this.state)
     }
 
     getEvents() {
@@ -67,8 +64,51 @@ export default class Browser extends Component {
             .catch(error => console.log('there is some error in retrieving... the error.', error))
     }
 
-    addEvent() {
-        console.log('add event is accessed...');
+    addEvent = (event) => {
+        event.preventDefault();
+        if (formvalid(this.state.formErrors)) {
+            console.log('valid form')
+            const newEvent = {
+                event_name: this.state.event_name,
+                event_details: this.state.event_details,
+                event_type: this.state.event_type,
+                location: this.state.location,
+                start_time: this.state.start_time,
+                end_time: this.state.end_time
+            };
+            Axios.post('http://localhost:3001/events/', newEvent)
+                .then(result => {
+                    console.log('the event has been added...')
+                    window.location.reload();
+                })
+                .catch(error => console.log('there is some error while adding the event: ', error))
+        } else {
+            console.log('form Invalid');
+        }
+    }
+
+    editEvent = (event) => {
+        event.preventDefault();
+        if (formvalid(this.state.formErrors)) {
+            console.log('valid form')
+            const newEvent = {
+                id: this.props.match.params.id,
+                event_name: this.state.event_name,
+                event_details: this.state.event_details,
+                event_type: this.state.event_type,
+                location: this.state.location,
+                start_time: this.state.start_time,
+                end_time: this.state.end_time
+            };
+            Axios.post('http://localhost:3001/events/' + newEvent.id, newEvent)
+                .then(result => {
+                    console.log('the event has been updated...')
+                    window.location.reload();
+                })
+                .catch(error => console.log('there is some error while updating the event: ', error))
+        } else {
+            console.log('form Invalid');
+        }
     }
 
 
@@ -110,9 +150,12 @@ export default class Browser extends Component {
                     </div>
                     <div className="col-sm-12 mb-3">
                         <div className='row'>&nbsp;&nbsp;&nbsp;
-                        <input type="text" id="myFilter" className="form-control col-sm-10" onChange={this.handleChange} placeholder="Search for keyword.." />
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap" >Create an Event</button></div>
-                        <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <input type="text" id="myFilter" className="form-control col-sm-10" onChange={this.handleChange} 
+                                placeholder="Search for keyword.." /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <button type="button" className="btn btn-primary" data-toggle="modal"
+                                data-target="#exampleModal"  data-whatever="@getbootstrap" >Create an Event</button></div>
+                        <div className="modal fade" id="exampleModal" tabIndex="-1"
+                            role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div className="modal-dialog" role="document">
                                 <div className="modal-content">
                                     <div className="modal-header">
@@ -155,25 +198,28 @@ export default class Browser extends Component {
                                                     onChange={this.handleErrors} />
                                                 {formErrors.location.length > 0 && <span className='error text-danger'>{formErrors.location}</span>}
                                             </div>
-                                            <div class="input-group date" data-provide="datepicker">
-                                            <label>Start Date</label>
-                                                <input type="text" className="form-control" />
-                                                <div className="input-group-addon">
-                                                <DatePicker id="example-datepicker" value={this.state.start_time}/>
-                                                </div>
-                                            </div>
-                                            {/* <div className="form-group">
-                                                <label for="recipient-name" className="col-form-label">Recipient:</label>
-                                                <input type="text" className="form-control" id="recipient-name" />
+                                            <div className="form-group">
+                                                <label>Start Date</label>
+                                                <DatePicker
+                                                    selected={this.state.start_time}
+                                                    onChange={date => this.setState({ start_time: date })}
+                                                    dateFormat="dd-mm-yyyy"
+                                                    name="start_time"
+                                                    className="form-control"
+                                                />
                                             </div>
                                             <div className="form-group">
-                                                <label for="message-text" className="col-form-label">Message:</label>
-                                                <textarea className="form-control" id="message-text"></textarea>
-                                            </div> */}
-
-                                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-          &nbsp;&nbsp;&nbsp;
-          <button type="submit" className="btn btn-primary">Submit</button>
+                                                <label>End Date</label>
+                                                <DatePicker
+                                                    selected={this.state.end_time}
+                                                    onChange={date => this.setState({ end_time: date })}
+                                                    dateFormat="dd-mm-yyyy"
+                                                    name="end_time"
+                                                    className="form-control"
+                                                />
+                                            </div>
+                                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>&nbsp;&nbsp;&nbsp;
+                                            <button type="submit" className="btn btn-primary" >Submit</button>
                                         </form>
                                     </div>
                                 </div>
@@ -201,8 +247,82 @@ export default class Browser extends Component {
                                                 <p className='lead'><b>Location -</b>&nbsp;{listValue.location}</p>
                                                 <p className='lead'><b>Start-Date :</b>&nbsp;{listValue.start_time}</p>
                                                 <p className='lead'><b>End-Date :</b>&nbsp;{listValue.end_time}</p><br /><br />
-                                                <button className="btn btn-secondary">Edit</button>&nbsp;&nbsp;
+                                                <button className="btn btn-secondary" data-toggle="modal" data-target={"#editModal" + index} data-whatever="@getbootstrap" >Edit</button>&nbsp;&nbsp;
                                                 <button className="btn btn-danger" onClick={this.deleteEvent.bind(this, listValue.id)}>Delete</button>
+                                                <div className="modal fade" id={"editModal" + index} tabIndex="-1" role="dialog" aria-labelledby={"exampleModalLabel" + index} aria-hidden="true">
+                                                    <div className="modal-dialog" role="document">
+                                                        <div className="modal-content">
+                                                            <div className="modal-header">
+                                                                <h5 className="modal-title" id={"exampleModalLabel" + index}>Edit {listValue.event_name}</h5>
+                                                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                                                    <span aria-hidden="true">&times;</span>
+                                                                </button>
+                                                            </div>
+                                                            <div className="modal-body">
+                                                                <form onSubmit={this.editEvent}>
+                                                                    <div className='form-group'>
+                                                                        <label>Event Name</label>
+                                                                        <input required type="text"
+                                                                            className={`form-control ${formErrors.event_name.length > 0 ? 'is-invalid' : null}`}
+                                                                            name="event_name"
+                                                                            value={listValue.event_name || ''}
+                                                                            onChange={this.handleErrors} />
+                                                                        {formErrors.event_name.length > 0 && <span className='error text-danger'>{formErrors.event_name}</span>}
+                                                                    </div>
+                                                                    <div className='form-group'>
+                                                                        <label>Event Details</label>
+                                                                        <textarea required type="text"
+                                                                            className={`form-control ${formErrors.event_details.length > 0 ? 'is-invalid' : null}`}
+                                                                            name="event_details"
+                                                                            value={listValue.event_details || ''}
+                                                                            onChange={this.handleErrors} />
+                                                                        {formErrors.event_details.length > 0 && <span className='error text-danger'>{formErrors.event_details}</span>}
+                                                                    </div>
+                                                                    <div className='form-group'>
+                                                                        <label>Event Type</label>
+                                                                        <input required type="text"
+                                                                            className={`form-control ${formErrors.event_type.length > 0 ? 'is-invalid' : null}`}
+                                                                            name="event_type"
+                                                                            value={listValue.event_type || ''}
+                                                                            onChange={this.handleErrors} />
+                                                                        {formErrors.event_type.length > 0 && <span className='error text-danger'>{formErrors.event_type}</span>}
+                                                                    </div>
+                                                                    <div className='form-group'>
+                                                                        <label>Event Location</label>
+                                                                        <input required type="text"
+                                                                            className={`form-control ${formErrors.location.length > 0 ? 'is-invalid' : null}`}
+                                                                            name="location"
+                                                                            value={listValue.location || ''}
+                                                                            onChange={this.handleErrors} />
+                                                                        {formErrors.location.length > 0 && <span className='error text-danger'>{formErrors.location}</span>}
+                                                                    </div>
+                                                                    <div className="form-group">
+                                                                        <label>Start Date</label>
+                                                                        <DatePicker required
+                                                                            // selected={listValue.start_time}
+                                                                            onChange={date => this.setState({ start_time: date })}
+                                                                            dateFormat="dd-mm-yyyy"
+                                                                            name="start_time"
+                                                                            className="form-control"
+                                                                        />
+                                                                    </div>
+                                                                    <div className="form-group">
+                                                                        <label>End Date</label>
+                                                                        <DatePicker required
+                                                                            // selected={listValue.end_time}
+                                                                            onChange={date => this.setState({ end_time: date })}
+                                                                            dateFormat="dd-mm-yyyy"
+                                                                            name="end_time"
+                                                                            className="form-control"
+                                                                        />
+                                                                    </div>
+                                                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>&nbsp;&nbsp;&nbsp;
+                                                                    <button type="submit" className="btn btn-primary" >Submit</button>
+                                                                </form>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Main from './components/Main';
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import "../node_modules/react-datepicker/dist/react-datepicker.css";
 
 ReactDOM.render(
   <React.StrictMode>
